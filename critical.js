edb671f2c5b15bcabda5c276b689aebf3d41a906
frenchavandal <42499534+frenchvandal@users.noreply.github.com>
2021-05-23 10:18:49 +0200

    tet

---
+++ critical.js
@@ -0,0 +1,29 @@
+const critical = require("critical");
+
+critical.generate(
+  {
+    base: "public/",
+    css: [
+      "public/css/base.min.d34db1546050ff69f503cf2518439fcf67b079db9e982f7ddaa98992bbfe0ec1.css",
+    ],
+    src: "index.html",
+    inline: true,
+    dimensions: [
+      {
+        height: 500,
+        width: 300,
+      },
+      {
+        height: 720,
+        width: 1280,
+      },
+    ],
+  },
+  (err, output) => {
+    if (err) {
+      console.error(err);
+    } else if (output) {
+      console.log("Generated critical CSS");
+    }
+  }
+);
