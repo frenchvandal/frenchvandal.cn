206260604cccc83e2c6fa3bbcccf82f0033b9548
frenchavandal <42499534+frenchvandal@users.noreply.github.com>
2021-05-23 09:57:41 +0200

    lol

--- themes/hugo-flex/assets/js/perf.js
+++
@@ -1,9 +0,0 @@
-const getLoadTime = () =>
-  (PerformanceNavigationTiming.domContentLoadedEventEnd -
-    PerformanceNavigationTiming.PerformanceEntry.startTime) /
-  1e3;
-window.addEventListener("load", () => {
-  document.querySelector(
-    ".js-loaded-in"
-  ).innerText = `- loaded in ${getLoadTime()}s`;
-});
