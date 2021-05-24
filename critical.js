7299b8b5962aa66b788c5a505c14d40ced181ea5
frenchvandal <42499534+frenchvandal@users.noreply.github.com>
2021-05-24 19:10:01 +0200

    Update critical.js

--- critical.js
+++ critical.js
@@ -4,7 +4,7 @@ critical.generate(
   {
     base: `${process.cwd()}/public/`,
     css: [
-      `${process.cwd()}/public/css/base.min.3fb4c3be945da2c8d9a72ca35e718facbadf2aca3b3ca1daefb9641a808076cb.css`,
+      `${process.cwd()}/public/css/base.min.a9d54e92b230529e39493dd2cee5ff238fe74bb3a009528b9558211b6f8042c3aae2408ecbdadb0a3a5ca7bd6f776108100c76540bc79d9a9cecbc4209c8d0c4.css`,
     ],
     src: "index.html",
     target: "index.html",
